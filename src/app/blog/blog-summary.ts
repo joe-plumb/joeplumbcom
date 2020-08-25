36a5e8970c3ab754e744a5f06b279eff33ec765e
Joe Plumb <hello@joeplumb.com>
2020-08-25 03:38:21 +0100

    added serverless blog post

--- src/app/blog/blog-summary.ts
+++ src/app/blog/blog-summary.ts
@@ -1,4 +1,5 @@
 import { Posts } from '../posts/Posts';
 
 export const POSTS: Posts[] = [
+  { id: 12, urltitle: 'serverless-functions-with-azure-and-python', title: 'Serverless functions with Azure and Python', summary: 'I\'ve been working with Azure Functions since late last year - my first real opportunity to work ...' },
   { id: 11, urltitle: 'hello-world', title: '✨Blog v0.2 is now live!✨', summary: 'Here we are then! First blog post going live in true mvp style. I am still adding to the structure...' }];
